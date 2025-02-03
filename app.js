cecea9a9f8d9d26bc01f337458548596feac0dbd
Michael Ziliani <zililanimichael7@gmail.com>
2025-02-03 14:58:15 -0500

    Corrigiendo errores en funcion agregar-amigo

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Array para almacenar los nombres de amigo
+//Lista de amigos.
 let amigos = [];
 
+//Función para agragar amigos a la lista.
 function agregarAmigo () {
-    //Captura el valor del campo de entrada.
-    let amigoNuevo = document.getElementById('amigo').value;
+    let nuevoAmigo = document.querySelector('#amigo');
+    let nombre = nuevoAmigo.value.trim()
 
-    //Valida que le campo de entrada no esté vacío.
-    if (amigoNuevo.trim() === '') {
-        document.querySelector('#amigo').placeholder = 'Por favor, ingresa un nombre válido';
-    } else {
-        //Agrega a la lista el nombre ingresado.
-        amigos.push(amigoNuevo);
-        console.log(amigos);
-
-        //Limpia el campo de entrada despues de agregar el nombre.
-        document.querySelector('#amigo').value = '';    
-        document.getElementById('amigo').placeholder = 'Escribe un nombre';
+    if (nombre === '') {
+        document.getElementById('amigo').placeholder = 'Por favor, ingrese un nombre válido';
+        return;
     }
+
+    amigos.push(nombre);
+    nuevoAmigo.value = ''; 
+    document.getElementById('amigo').placeholder = 'Escribe un nombre';
 }
